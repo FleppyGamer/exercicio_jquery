950fa2c288d29ae555054d9c3e1bf775df92f814
Fleppy_Gamer <fleppyg@gmail.com>
2024-01-04 17:50:03 -0300

    Choir: Choir checker and unchecker added

--- js/main.js
+++ js/main.js
@@ -6,10 +6,25 @@ $(document).ready(function (){
 
     function choirAdder (){
         const text = $('#text').val();
-        const newChoir = $('<li id="item"></li>')
+        const newChoir = $('<li id="item" title="Clique para marcar a atividade"></li>');
         $(`<a>${text}</a>`).appendTo(newChoir);
-        $(newChoir).appendTo('ol')
+        $(newChoir).appendTo('ol');
 
         $('#text').val('');
+
+        newChoir.click(function(e){
+            e.preventDefault();
+    
+            newChoir.attr("class","checked");
+            newChoir.attr('title', 'Dê um clique duplo para desmarcar a atividade')
+        })
+        
+        newChoir.dblclick(function(e){
+            e.preventDefault();
+    
+            newChoir.attr("class"," ")
+            newChoir.attr('title', 'Clique para marcar a atividade')
+        })
     }
+
 })
